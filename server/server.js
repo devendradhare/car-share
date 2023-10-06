380b74db2975a395eb90dc1cb5ea9eec7565fe45
devendra dhare <devendradhare03@gmail.com>
2023-10-06 13:46:57 +0530

    server setup with express

--- server/server.js
+++ server/server.js
@@ -1,1 +1,17 @@
-// server setup
+const path = require("path")
+
+const express = require("express")
+const app = express()
+const PORT = 4000
+console.log(__dirname);
+
+app.use(express.static(path.join(__dirname, "../client/build")))
+
+app.get("*", (req, res) => {
+    res.send("hello world")
+})
+
+app.listen(PORT, () => {
+  console.log("listening on port", PORT);
+//   console.log("listening on port", PORT, "http://localhost:" + PORT)
+});
